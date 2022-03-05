47eb82d11ec403e6fcff84935b2595679cfbdc4e
171035726@qq.com <171035726@qq.com>
2022-03-05 23:16:22 +0800

    feat: 2022-03-05

---
+++ src/medium/16. 最接近的三数之和.ts
@@ -0,0 +1,35 @@
+function threeSumClosest(nums: number[], target: number): number {
+    // 1. 排序
+    nums.sort((a, b) => a - b);
+    const len = nums.length;
+    // 2. 计算初始和
+    let ans = nums[0] + nums[1] + nums[2];
+
+    // 3. 遍历
+    for (let i = 0; i < len; i++) {
+        const cur = nums[i];
+        let left = i + 1, right = len - 1;
+
+        // 当前项 + 后一项 + 最后一项
+        // left 向后走
+        // right 向前走
+        while (left < right) {
+            const sum = cur + nums[left] + nums[right];
+
+            if (Math.abs(target - sum) < Math.abs(target - ans)) {
+                ans = sum;
+            }
+
+            // 如果相等
+            if (sum === target) {
+                return target
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+
+    return ans;
+};
