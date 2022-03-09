e71d8eb7fb248c9631dadfbb5d9b65ebb809563e
171035726@qq.com <171035726@qq.com>
2022-03-09 20:32:55 +0800

    feat: 2022-03-09

---
+++ src/medium/22. 括号生成.ts
@@ -0,0 +1,33 @@
+/**
+    数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+    示例 1：
+    输入：n = 3
+    输出：["((()))","(()())","(())()","()(())","()()()"]
+
+    示例 2：
+    输入：n = 1
+    输出：["()"]
+*/
+
+function generateParenthesis(n: number): string[] {
+    const ret: string[] = [];
+
+    function dfs(left: number, right: number, str: string): void {
+        if (left === 0 && right === 0) {
+            ret.push(str);
+            return;
+        }
+
+        if (left > 0) {
+            dfs(left - 1, right, str + "(");
+        }
+
+        if (right > left) {
+            dfs(left, right - 1, str + ")");
+        }
+    }
+
+    dfs(n, n, "");
+    return ret;
+};
