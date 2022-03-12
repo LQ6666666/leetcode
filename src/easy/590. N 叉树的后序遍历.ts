8f569d39ddf77bfa526bac3c1379b94af06f8b9f
171035726@qq.com <171035726@qq.com>
2022-03-12 19:45:19 +0800

    feat: 2022-03-12

---
+++ src/easy/590. N 叉树的后序遍历.ts
@@ -0,0 +1,25 @@
+class Node {
+    val: number
+    children: Node[]
+    constructor(val?: number) {
+        this.val = (val === undefined ? 0 : val)
+        this.children = []
+    }
+}
+
+function postorder(root: Node | null): number[] {
+    if (root === null) return [];
+
+    const ret = [];
+
+    for (const item of root.children) {
+        ret.push(...postorder(item));
+    }
+
+    ret.push(root.val);
+
+    return ret;
+};
+
+
+export { };
