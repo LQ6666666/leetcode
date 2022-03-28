c66d1e8dfe69f56284d0acbf50d3ef9ea6f641c1
171035726@qq.com <171035726@qq.com>
2022-03-28 09:00:13 +0800

    feat: 2022-03-28

---
+++ src/medium/2028. 找出缺失的观测数据.ts
@@ -0,0 +1,26 @@
+function missingRolls(rolls: number[], mean: number, n: number): number[] {
+    const m = rolls.length;
+    // 总投掷次数
+    const count = m + n;
+    // 已知投掷点数
+    const known = rolls.reduce((prev, cur) => prev + cur, 0);
+    // 总点数
+    const total = count * mean;
+    // 待分配点数
+    const unknown = total - known;
+    // edge case
+    if (unknown > 6 * n || unknown < n) {
+        return [];
+    }
+
+    // 平均待分配的点数
+    let base = Math.floor(unknown / n);
+    let remainder = unknown % n;
+
+    const ans = Array.from({ length: n }, () => base);
+    for (let i = 0; remainder > 0; remainder--, i++) {
+        ans[i]++;
+    }
+
+    return ans;
+};
