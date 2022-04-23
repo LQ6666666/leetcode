7d73de2d0928bbfa146b2917328444f9078eaec7
171035726@qq.com <171035726@qq.com>
2022-04-23 15:28:21 +0800

    feat: 2022-04-23

--- src/medium/540. 有序数组中的单一元素.ts
+++ src/medium/540. 有序数组中的单一元素.ts
@@ -1,15 +1,41 @@
+// function singleNonDuplicate(nums: number[]): number {
+//     let i = 0, j = 1;
+//     const len = nums.length;
+
+//     while (j < len) {
+//         if (nums[i] !== nums[j]) {
+//             return nums[i];
+//         }
+
+//         i += 2;
+//         j += 2;
+//     }
+
+//     return nums[0];
+// };
+
 function singleNonDuplicate(nums: number[]): number {
-    let i = 0, j = 1;
-    const len = nums.length;
+    let left = 0, right = nums.length - 1;
 
-    while (j < len) {
-        if (nums[i] !== nums[j]) {
-            return nums[i];
+    while (left <= right) {
+        const middle = left + ((right - left) >> 1);
+
+        if (middle % 2 === 0) {
+            if (nums[middle + 1] === nums[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        } else {
+            if (nums[middle - 1] === nums[middle]) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
         }
-
-        i += 2;
-        j += 2;
     }
 
-    return nums[0];
+    return left;
 };
+
+export { };
