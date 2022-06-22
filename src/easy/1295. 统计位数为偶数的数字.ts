0ae9699e55cd84b1e53a71bcda0b82aac6522546
lq <171035726@qq.com>
2022-06-22 20:55:31 +0800

    feat: 2022-06-22

---
+++ src/easy/1295. 统计位数为偶数的数字.ts
@@ -0,0 +1,18 @@
+function findNumbers(nums: number[]): number {
+    let count = 0;
+    for (let num of nums) {
+        let n = 0;
+        while (num > 0) {
+            num = Math.floor(num / 10);
+            n++;
+        }
+
+        if (n % 2 === 0) {
+            count++;
+        }
+    }
+    return count;
+};
+
+
+export { };
