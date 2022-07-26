3c34021397bff6dc077d57e731b66b76a169debc
lq <171035726@qq.com>
2022-07-26 23:41:31 +0800

    feat: 2022-07-26

--- src/medium/3. 无重复字符的最长子串.ts
+++ src/medium/3. 无重复字符的最长子串.ts
@@ -16,21 +16,45 @@
 //     return Array.from<number>(set).sort((a, b) => b - a)[0] ?? 0;
 // };
 
+// function lengthOfLongestSubstring(s: string): number {
+//     let index: number = 0;
+//     let result: number = 0;
+
+//     for (let i = 0, sLength = s.length; i < sLength; i++) {
+//         const position: number = s.indexOf(s[i], index);
+
+//         if (position < i) {
+//             index = position + 1;
+//         } else {
+//             result = Math.max(result, i - index + 1);
+//         }
+//     }
+
+//     return result;
+// };
+
+
 function lengthOfLongestSubstring(s: string): number {
-    let index: number = 0;
-    let result: number = 0;
+    const n = s.length;
+    let left = 0, right = 0;
+    const set = new Set<string>();
+    let ans = 0;
 
-    for (let i = 0, sLength = s.length; i < sLength; i++) {
-        const position: number = s.indexOf(s[i], index);
-
-        if (position < i) {
-            index = position + 1;
-        } else {
-            result = Math.max(result, i - index + 1);
+    while (right < n) {
+        if (set.has(s[left])) {
+            set.delete(s[left]);
+            left++;
         }
+
+        while (!set.has(s[right]) && right < n) {
+            set.add(s[right]);
+            right++;
+        }
+
+        ans = Math.max(ans, set.size);
     }
 
-    return result;
+    return ans;
 };
 
 
