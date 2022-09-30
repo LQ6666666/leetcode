1a7c98e024e9910ebd524d7b9d95a2db3568aee5
李琦 <171035726@qq.com>
2022-09-30 10:26:02 +0800

    feat: 2022-09-30

---
+++ src/other/面试题 01.08.零矩阵.ts
@@ -0,0 +1,24 @@
+function setZeroes(matrix: number[][]): void {
+  const m = matrix.length, n = matrix[0].length;
+  const row = new Array(m).fill(0);
+  const col = new Array(n).fill(0);
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        row[i] = 1;
+        col[j] = 1;
+      }
+    }
+  }
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (row[i] || col[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+};
+
+export { };
