0873e85df060abd70df5f3fc55b87cf60824fe98
lq <171035726@qq.com>
2022-11-27 15:50:53 +0800

    feat: 2022-11-27

---
+++ src/easy/1752.检查数组是否经排序和轮转得到.ts
@@ -0,0 +1,12 @@
+function check(nums: number[]): boolean {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[(i + 1) % nums.length]) {
+      count++;
+    }
+  }
+
+  return count <= 1;
+}
+
+export { check };
