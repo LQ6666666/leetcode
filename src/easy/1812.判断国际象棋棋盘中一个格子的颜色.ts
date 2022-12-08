d624c31bae4bf012a07eb6d95dbebcfd3fdb95eb
李琦 <171035726@qq.com>
2022-12-08 10:08:44 +0800

    feat: 2022-12-08

---
+++ src/easy/1812.判断国际象棋棋盘中一个格子的颜色.ts
@@ -0,0 +1,17 @@
+function squareIsWhite(coordinates: string): boolean {
+  // 1.构建棋盘
+  const chessboard = Array.from({ length: 8 }, (_, y) =>
+    Array.from({ length: 8 }, (_, x) => {
+      const result = x % 2 === 0;
+
+      return y % 2 === 0 ? result : !result;
+    })
+  );
+
+  const col = coordinates[0].charCodeAt(0) - 97;
+  const row = 8 - +coordinates[1];
+
+  return chessboard[row][col];
+}
+
+export { squareIsWhite };
