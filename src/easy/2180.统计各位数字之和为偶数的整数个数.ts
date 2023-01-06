84f4517f1c1841f4f171cb2d8a12075120779a5a
lq <171035726@qq.com>
2023-01-06 22:40:57 +0800

    feat: 2023-1-6

---
+++ src/easy/2180.统计各位数字之和为偶数的整数个数.ts
@@ -0,0 +1,18 @@
+function countEven(num: number): number {
+  let ans = 0;
+  for (let i = 2; i <= num; i++) {
+    let x = i;
+    let sum = 0;
+    while (x > 0) {
+      sum += x % 10;
+      x = Math.floor(x / 10);
+    }
+
+    if (sum % 2 === 0) {
+      ans++;
+    }
+  }
+  return ans;
+}
+
+export {};
