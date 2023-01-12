190b9098dc34bec9f77b1ad6b63df83431466f25
李琦 <171035726@qq.com>
2023-01-12 09:48:20 +0800

    feat: 2023-1-12

--- src/medium/1807. 替换字符串中的括号内容.ts
+++ src/medium/1807. 替换字符串中的括号内容.ts
@@ -1,29 +1,64 @@
+function evaluate1(s: string, knowledge: string[][]): string {
+  let ans = "";
+  const map = new Map<string, string>();
+  for (const item of knowledge) {
+    map.set(item[0], item[1]);
+  }
+
+  let temp = "",
+    flag = false;
+  for (const char of s) {
+    if (char === "(") {
+      flag = true;
+    } else if (char === ")") {
+      flag = false;
+      if (map.has(temp)) {
+        ans += map.get(temp);
+      } else {
+        ans += "?";
+      }
+
+      temp = "";
+    } else if (flag) {
+      temp += char;
+    } else {
+      ans += char;
+    }
+  }
+
+  return ans;
+}
+
 function evaluate(s: string, knowledge: string[][]): string {
-    let ans = "";
-    const map = new Map<string, string>();
-    for (const item of knowledge) {
-        map.set(item[0], item[1]);
+  const map = new Map<string, string>();
+  for (const [key, value] of knowledge) {
+    map.set(key, value);
+  }
+
+  let ans = "";
+  const n = s.length;
+  let i = 0;    
+
+  while (i < n) {
+    let ch = s[i++];
+
+    if (ch === "(") {
+      let key = "";
+      ch = s[i];
+      while (ch !== ")") {
+        key += ch;
+        ch = s[++i];
+      }
+
+      const value = map.get(key) ?? "?";
+      ans += value;
+      i++;
+    } else {
+      ans += ch;
     }
+  }
 
-    let temp = "", flag = false;
-    for (const char of s) {
-        if (char === "(") {
-            flag = true;
-        } else if (char === ")") {
-            flag = false;
-            if (map.has(temp)) {
-                ans += map.get(temp);
-            } else {
-                ans += "?";
-            }
+  return ans;
+}
 
-            temp = "";
-        } else if (flag) {
-            temp += char;
-        } else {
-            ans += char;
-        }
-    }
-
-    return ans;
-};
+export { evaluate };
