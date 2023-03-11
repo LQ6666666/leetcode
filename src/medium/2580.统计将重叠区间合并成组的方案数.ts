b9c5aa8d1784cc525b773247b6d5a679f22cf4a4
lq <171035726@qq.com>
2023-03-11 21:09:15 +0800

    feat: 2023-03-11

---
+++ src/medium/2580.统计将重叠区间合并成组的方案数.ts
@@ -0,0 +1,24 @@
+import { MOD } from "@/utils";
+
+function countWays(ranges: number[][]): number {
+  const n = ranges.length;
+  ranges.sort(([a], [b]) => a - b);
+
+  let count = 0;
+  let maxRange = ranges[0][1];
+  for (let i = 1; i < n; i++) {
+    const range = ranges[i];
+    if (range[0] > maxRange) {
+      count++;
+    }
+    maxRange = Math.max(range[1], maxRange);
+  }
+
+  let ans = 1;
+  while (count >= 0) {
+    ans = (ans * 2) % MOD;
+  }
+  return ans;
+}
+
+export {};
