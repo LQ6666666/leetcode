497ce5286d3383dc43c6e09ff020bcbacf9a0438
lq <171035726@qq.com>
2023-03-14 20:47:00 +0800

    feat: 2023-03-14

---
+++ src/easy/2586.统计范围内的元音字符串数.ts
@@ -0,0 +1,14 @@
+const vowelSet = new Set("aeiou");
+
+function vowelStrings(words: string[], left: number, right: number): number {
+  let ans = 0;
+  for (let i = left; i <= right; i++) {
+    const word = words[i];
+    if (vowelSet.has(word[0]) && vowelSet.has(word[word.length - 1])) {
+      ans++;
+    }
+  }
+  return ans;
+}
+
+export {};
