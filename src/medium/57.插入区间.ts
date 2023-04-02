77df5925866b5cf48bb08e95af8c718531fa1b34
lq <171035726@qq.com>
2023-04-02 14:01:05 +0800

    feat: 2023-04-02

---
+++ src/medium/57.插入区间.ts
@@ -0,0 +1,24 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  const n = intervals.length;
+  let index = 0;
+  const result: number[][] = [];
+  // 找到需要合并的区间
+  while (index < n && intervals[index][1] < newInterval[0]) {
+    result.push(intervals[index++]);
+  }
+
+  // 合并区间
+  while (index < n && intervals[index][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
+    newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
+    index++;
+  }
+
+  result.push(newInterval);
+  while (index < n) {
+    result.push(intervals[index++]);
+  }
+  return result;
+}
+
+export {};
