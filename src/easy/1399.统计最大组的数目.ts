6e9c72edef8945ba252bcfe9cbac04ee87ef682f
lq <171035726@qq.com>
2023-04-15 17:02:07 +0800

    feat: 2023-04-15

---
+++ src/easy/1399.统计最大组的数目.ts
@@ -0,0 +1,27 @@
+function countLargestGroup(n: number): number {
+  const counts = new Array<number>(37).fill(0);
+
+  let max = 0;
+  for (let i = 1; i <= n; i++) {
+    let x = i;
+    let sum = 0;
+    while (x >= 10) {
+      sum += x % 10;
+      x = Math.floor(x / 10);
+    }
+    sum += x;
+    counts[sum]++;
+    max = Math.max(counts[sum], max);
+  }
+
+  let ans = 0;
+  for (let i = 1; i < 37; i++) {
+    if (max === counts[i]) {
+      ans++;
+    }
+  }
+
+  return ans;
+}
+
+export {};
