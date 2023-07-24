4f14329395c280e4fb575a0a37e3e2f95c925bea
李琦 <171035726@qq.com>
2023-07-24 11:09:33 +0800

    feat: 2023-07-24

---
+++ src/easy/771.宝石与石头.ts
@@ -0,0 +1,14 @@
+function numJewelsInStones(jewels: string, stones: string): number {
+  const set = new Set(jewels);
+
+  let ans = 0;
+  for (const stone of stones) {
+    if (set.has(stone)) {
+      ans++;
+    }
+  }
+
+  return ans;
+}
+
+export {};
