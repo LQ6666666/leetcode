8cf5b3f3b8f287f2ae724827d82bba533928d7e2
lq <171035726@qq.com>
2023-07-29 21:12:44 +0800

    feat: 2023-07-29

---
+++ src/easy/141.环形链表.ts
@@ -0,0 +1,36 @@
+import { ListNode } from "../utils";
+
+function hasCycle$(head: ListNode | null): boolean {
+  if (head === null || head.next === null) return false;
+
+  // 慢的走一步，快的走两步
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head.next;
+
+  while (slow !== null && fast !== null) {
+    if (slow === fast) return true;
+
+    slow = slow.next;
+    fast = fast.next?.next ?? null;
+  }
+
+  return false;
+}
+
+function hasCycle(head: ListNode | null): boolean {
+  if (head === null || head.next === null) return false;
+
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+
+  while (slow !== null && fast !== null) {
+    slow = slow.next;
+    fast = fast.next?.next ?? null;
+
+    if (slow === fast) return true;
+  }
+
+  return false;
+}
+
+export {};
