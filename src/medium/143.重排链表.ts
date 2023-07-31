e177907cc2536791c51e8dbcb97ac3489c42673d
lq <171035726@qq.com>
2023-07-31 23:14:03 +0800

    feat: 2023-07-31

--- src/medium/143.重排链表.ts
+++ src/medium/143.重排链表.ts
@@ -1,6 +1,6 @@
 import { ListNode } from "@/utils";
 
-function reorderList(head: ListNode | null): void {
+function reorderList$(head: ListNode | null): void {
   // 找到中间的节点
   let fast = head;
   let slow = head;
@@ -34,6 +34,48 @@ function reorderList(head: ListNode | null): void {
     cur1!.next = node;
     node.next = next;
     cur1 = next;
+  }
+}
+
+function reorderList(head: ListNode | null): void {
+  let slow = head;
+  let fast = head;
+
+  // 1. 获取中间的节点
+  let prev: ListNode | null = null;
+  while (fast) {
+    prev = slow;
+    slow = slow!.next;
+    fast = fast.next?.next ?? null;
+  }
+
+  // 2. 断开中间和后续节点连接
+  if (prev) {
+    prev.next = null;
+    prev = null;
+  }
+
+  // 3. 反转后续的节点
+  let current = slow;
+  while (current) {
+    const { next } = current;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  // 插入节点
+  let l1 = head;
+  let l2 = prev;
+  while (l2) {
+    const l1Next = l1?.next ?? null;
+    const l2Next = l2?.next ?? null;
+
+    l1!.next = l2;
+    l2.next = l1Next;
+
+    l1 = l1Next;
+    l2 = l2Next;
   }
 }
 
