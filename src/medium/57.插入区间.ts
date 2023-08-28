3c2634317c1b5b88f90e3e68e30af6b549c6116d
lq <171035726@qq.com>
2023-08-28 22:33:58 +0800

    feat: 2023-08-28

--- src/medium/57.插入区间.ts
+++ src/medium/57.插入区间.ts
@@ -1,4 +1,4 @@
-function insert(intervals: number[][], newInterval: number[]): number[][] {
+function insert$(intervals: number[][], newInterval: number[]): number[][] {
   const n = intervals.length;
   let index = 0;
   const result: number[][] = [];
@@ -8,6 +8,27 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
   }
 
   // 合并区间
+  while (index < n && intervals[index][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
+    newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
+    index++;
+  }
+
+  result.push(newInterval);
+  while (index < n) {
+    result.push(intervals[index++]);
+  }
+  return result;
+}
+
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  const n = intervals.length;
+  let index = 0;
+  const result: number[][] = [];
+  while (index < n && intervals[index][1] < newInterval[0]) {
+    result.push(intervals[index++]);
+  }
+
   while (index < n && intervals[index][0] <= newInterval[1]) {
     newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
     newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
