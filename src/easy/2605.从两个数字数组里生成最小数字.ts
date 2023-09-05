d166c0f51f348c51bda79547b97003593ec711e3
李琦 <171035726@qq.com>
2023-09-05 10:16:22 +0800

    feat: 2023-09-05

--- src/easy/2605.从两个数字数组里生成最小数字.ts
+++ src/easy/2605.从两个数字数组里生成最小数字.ts
@@ -1,4 +1,4 @@
-function minNumber(nums1: number[], nums2: number[]): number {
+function minNumber$(nums1: number[], nums2: number[]): number {
   const mapA = new Array<boolean>(10).fill(false);
   const mapB = new Array<boolean>(10).fill(false);
   for (const num of nums1) {
@@ -25,6 +25,31 @@ function minNumber(nums1: number[], nums2: number[]): number {
   }
 
   return minA < minB ? minA * 10 + minB : minB * 10 + minA;
+}
+
+function minNumber(nums1: number[], nums2: number[]): number {
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
+
+  let min1 = Infinity;
+  let min2 = Infinity;
+  for (let i = 1; i < 10; i++) {
+    if (set1.has(i) && set2.has(i)) {
+      return i;
+    }
+
+    if (min1 === Infinity && set1.has(i)) {
+      min1 = i;
+    }
+
+    if (min2 === Infinity && set2.has(i)) {
+      min2 = i;
+    }
+  }
+
+  const a = Math.min(min1, min2);
+  const b = Math.max(min1, min2);
+  return a * 10 + b;
 }
 
 export { minNumber };
