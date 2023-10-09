08fb7fe4d37a475503fc7a56bf67723616e5a84c
李琦 <171035726@qq.com>
2023-10-09 14:03:32 +0800

    feat: 2578.最小和分割

--- src/easy/2578.最小和分割.ts
+++ src/easy/2578.最小和分割.ts
@@ -1,4 +1,4 @@
-function splitNum(num: number): number {
+function splitNum$(num: number): number {
   const nums: number[] = [];
   let x = num;
   while (x > 0) {
@@ -16,6 +16,41 @@ function splitNum(num: number): number {
   }
 
   return +num1 + +num2;
+}
+
+function splitNum(num: number): number {
+  const nums: number[] = [];
+  let x = num;
+  while (x > 0) {
+    nums.push(x % 10);
+    x = Math.floor(x / 10);
+  }
+  nums.sort((a, b) => a - b);
+
+  let n = nums.length;
+  const num1: number[] = [];
+  const num2: number[] = [];
+
+  for (let i = 0; i < n; i++) {
+    const num = nums[i];
+    if (i % 2 === 0) {
+      num1.push(num);
+    } else {
+      num2.push(num);
+    }
+  }
+
+  return toNum(num1) + toNum(num2);
+}
+
+function toNum(nums: number[]): number {
+  const n = nums.length;
+
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    ans += nums[i] * 10 ** (n - i - 1);
+  }
+  return ans;
 }
 
 export {};
