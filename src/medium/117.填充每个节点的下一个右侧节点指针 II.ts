3a2f80bb224a4ce688a27d88f52af565daec5636
李琦 <171035726@qq.com>
2023-11-03 09:05:29 +0800

    feat: 117.填充每个节点的下一个右侧节点指针 II

--- src/medium/117.填充每个节点的下一个右侧节点指针 II.ts
+++ src/medium/117.填充每个节点的下一个右侧节点指针 II.ts
@@ -1,1 +1,43 @@
-export { connect } from "./116.填充每个节点的下一个右侧节点指针";
+export class Node {
+  constructor(
+    public val: number = 0,
+    public left: Node | null = null,
+    public right: Node | null = null,
+    public next: Node | null = null
+  ) {}
+}
+
+function connect(root: Node | null): Node | null {
+  // 层序遍历
+  let queue: Node[] = [];
+  if (root) {
+    queue.push(root);
+  }
+
+  while (queue.length) {
+    const newQueue: Node[] = [];
+    let prev: Node | null = null;
+
+    for (const node of queue) {
+      if (prev !== null) {
+        prev.next = node;
+        prev = node;
+      }
+      prev = node;
+
+      if (node.left) {
+        newQueue.push(node.left);
+      }
+
+      if (node.right) {
+        newQueue.push(node.right);
+      }
+    }
+
+    queue = newQueue;
+  }
+
+  return root;
+}
+
+export { connect };
