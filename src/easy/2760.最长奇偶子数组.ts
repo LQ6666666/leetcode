576877f3cf4ac4887b60558c36a0310bf54e916b
李琦 <171035726@qq.com>
2023-11-16 11:41:13 +0800

    feat: 2760.最长奇偶子数组

--- src/easy/2760.最长奇偶子数组.ts
+++ src/easy/2760.最长奇偶子数组.ts
@@ -1,4 +1,4 @@
-function longestAlternatingSubarray(nums: number[], threshold: number): number {
+function longestAlternatingSubarray$(nums: number[], threshold: number): number {
   const n = nums.length;
   let ans = 0;
 
@@ -9,6 +9,25 @@ function longestAlternatingSubarray(nums: number[], threshold: number): number {
     while (j < n && nums[j] <= threshold && nums[j] % 2 !== nums[j - 1] % 2) {
       j++;
     }
+    ans = Math.max(ans, j - i);
+    i = j - 1;
+  }
+
+  return ans;
+}
+
+function longestAlternatingSubarray(nums: number[], threshold: number): number {
+  const n = nums.length;
+  let ans = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > threshold || nums[i] % 2 !== 0) continue;
+    let j = i + 1;
+
+    while (j < n && nums[j] <= threshold && nums[j] % 2 !== nums[j - 1] % 2) {
+      j++;
+    }
+
     ans = Math.max(ans, j - i);
     i = j - 1;
   }
