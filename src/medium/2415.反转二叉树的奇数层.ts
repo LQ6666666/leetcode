f918d242c28b1778ec2077db265a14b254b23a2c
李琦 <171035726@qq.com>
2023-12-15 16:33:12 +0800

    feat: 2415.反转二叉树的奇数层

--- src/medium/2415.反转二叉树的奇数层.ts
+++ src/medium/2415.反转二叉树的奇数层.ts
@@ -1,11 +1,11 @@
 import { TreeNode } from "src/utils";
 
-function reverseOddLevels(root: TreeNode | null): TreeNode | null {
+function reverseOddLevels$(root: TreeNode | null): TreeNode | null {
   if (root === null) return null;
 
   dfs(root.left, root.right);
   return root;
-};
+}
 
 function dfs(l: TreeNode | null, r: TreeNode | null, depth: number = 1) {
   if (l === null) return null;
@@ -20,7 +20,7 @@ function dfs(l: TreeNode | null, r: TreeNode | null, depth: number = 1) {
   dfs(l.right, r?.left ?? null, depth + 1);
 }
 
-function reverseOddLevels1(root: TreeNode | null): TreeNode | null {
+function reverseOddLevels$$(root: TreeNode | null): TreeNode | null {
   if (root === null) return null;
 
   let queue: TreeNode[] = [root];
@@ -50,5 +50,32 @@ function reverseOddLevels1(root: TreeNode | null): TreeNode | null {
   }
 
   return root;
-};
+}
+
+function reverseOddLevels(root: TreeNode | null): TreeNode | null {
+  let queue = root ? [root] : [];
+  let depth = 1;
+
+  while (queue.length) {
+    const nextQueue: TreeNode[] = [];
+
+    for (const node of queue) {
+      if (node.left) nextQueue.push(node.left);
+      if (node.right) nextQueue.push(node.right);
+    }
+
+    if (depth % 2 !== 0) {
+      const values = nextQueue.map(node => node.val).reverse();
+      for (let i = 0; i < nextQueue.length; i++) {
+        nextQueue[i].val = values[i];
+      }
+    }
+
+    queue = nextQueue;
+    depth++;
+  }
+
+  return root;
+}
+
 export { reverseOddLevels };
