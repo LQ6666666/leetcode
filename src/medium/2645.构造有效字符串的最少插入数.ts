4d18abb9f319ec5b395a00f9d2c0d2a189106e43
李琦 <171035726@qq.com>
2024-01-11 09:36:18 +0800

    feat: 2645.构造有效字符串的最少插入数

--- src/medium/2645.构造有效字符串的最少插入数.ts
+++ src/medium/2645.构造有效字符串的最少插入数.ts
@@ -1,4 +1,4 @@
-function addMinimum(word: string): number {
+function addMinimum$(word: string): number {
   const chArr = ["a", "b", "c"];
   const n = word.length;
 
@@ -20,6 +20,35 @@ function addMinimum(word: string): number {
     ans += 2;
   } else if (lastCh === "b") {
     ans += 1;
+  }
+
+  return ans;
+}
+
+function addMinimum(word: string): number {
+  const n = word.length;
+  let i = 0;
+  let j: "a" | "b" | "c" = "a";
+
+  let ans = 0;
+  while (i < n) {
+    if (word[i] !== j) {
+      ans++;
+    } else {
+      i++;
+    }
+
+    if (j === "a") {
+      j = "b";
+    } else if (j === "b") {
+      j = "c";
+    } else {
+      j = "a";
+    }
+  }
+
+  if (j !== "a") {
+    ans += j === "b" ? 2 : 1;
   }
 
   return ans;
