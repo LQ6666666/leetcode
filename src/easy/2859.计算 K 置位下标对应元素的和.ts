aad31607e1c77ac60d75335792f897b2a190e4b4
李琦 <171035726@qq.com>
2024-01-25 14:31:44 +0800

    feat: 2859.计算 K 置位下标对应元素的和

--- src/easy/2859.计算 K 置位下标对应元素的和.ts
+++ src/easy/2859.计算 K 置位下标对应元素的和.ts
@@ -1,4 +1,4 @@
-function sumIndicesWithKSetBits(nums: number[], k: number): number {
+function sumIndicesWithKSetBits$(nums: number[], k: number): number {
   const n = nums.length;
   let ans = 0;
 
@@ -12,6 +12,28 @@ function sumIndicesWithKSetBits(nums: number[], k: number): number {
     if (count === k) {
       ans += nums[i];
     }
+  }
+
+  return ans;
+}
+
+function sumIndicesWithKSetBits(nums: number[], k: number): number {
+  let ans = 0;
+
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    let x = i;
+    let count = 0;
+    while (x) {
+      if (x & 1) {
+        count++;
+      }
+
+      if (count > k) break;
+
+      x = x >> 1;
+    }
+    ans += count === k ? nums[i] : 0;
   }
 
   return ans;
