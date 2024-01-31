964df2641ec2bfd59943087e0529ad78723c356a
李琦 <171035726@qq.com>
2024-01-31 09:22:04 +0800

    feat: 2670.找出不同元素数目差数组

--- src/easy/2670.找出不同元素数目差数组.ts
+++ src/easy/2670.找出不同元素数目差数组.ts
@@ -1,4 +1,4 @@
-function distinctDifferenceArray(nums: number[]): number[] {
+function $(nums: number[]): number[] {
   const n = nums.length;
   const ans = new Array<number>(n);
   const set = new Set<number>();
@@ -18,6 +18,35 @@ function distinctDifferenceArray(nums: number[]): number[] {
     }
   }
 
+  return ans;
+}
+
+const N = 55;
+function distinctDifferenceArray(nums: number[]): number[] {
+  const n = nums.length;
+  const prefix = new Array<number>(n).fill(0);
+  const suffix = new Array<number>(n).fill(0);
+
+  const preCnt = new Array<number>(N).fill(0);
+  const sufCnt = new Array<number>(N).fill(0);
+  for (let i = 0, j = n - 1, pc = 0, sc = 0; i < n && j >= 0; i++, j--) {
+    if (preCnt[nums[i]] === 0) {
+      preCnt[nums[i]] = 1;
+      pc++;
+    }
+    prefix[i] = pc;
+
+    if (sufCnt[nums[j]] === 0) {
+      sufCnt[nums[j]] = 1;
+      sc++;
+    }
+    suffix[j] = sc;
+  }
+
+  const ans = new Array<number>(n);
+  for (let i = 0; i < n; i++) {
+    ans[i] = prefix[i] - (i + 1 < n ? suffix[i + 1] : 0);
+  }
   return ans;
 }
 
