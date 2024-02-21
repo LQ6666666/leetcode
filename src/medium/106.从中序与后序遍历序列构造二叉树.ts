874ba8bb61a8597e59dcfd7f834412049ee1558b
李琦 <171035726@qq.com>
2024-02-21 09:03:26 +0800

    feat: 106.从中序与后序遍历序列构造二叉树

--- src/medium/106.从中序与后序遍历序列构造二叉树.ts
+++ src/medium/106.从中序与后序遍历序列构造二叉树.ts
@@ -1,6 +1,6 @@
 import { TreeNode } from "../utils";
 
-function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+function buildTree$(inorder: number[], postorder: number[]): TreeNode | null {
   const n = inorder.length;
   if (n === 0) return null;
 
@@ -29,6 +29,29 @@ function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
   let postIndex = n - 1;
 
   return helper(0, postIndex);
+}
+
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+  const n = postorder.length;
+  if (n === 0) return null;
+
+  const nodeValue = postorder[n - 1];
+  const index = inorder.indexOf(nodeValue);
+  if (index === -1) return null;
+
+  const node = new TreeNode(nodeValue);
+
+  const leftInorder = inorder.slice(0, index);
+  if (leftInorder.length) {
+    node.left = buildTree(leftInorder, postorder.slice(0, leftInorder.length));
+  }
+
+  const rightInorder = inorder.slice(index + 1);
+  if (rightInorder.length) {
+    node.right = buildTree(rightInorder, postorder.slice(leftInorder.length, -1));
+  }
+
+  return node;
 }
 
 export { buildTree };
