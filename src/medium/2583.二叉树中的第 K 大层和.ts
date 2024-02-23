8e0c567a5a9666c67e7129a3200d96a1f646d931
李琦 <171035726@qq.com>
2024-02-23 15:04:35 +0800

    feat: 2583.二叉树中的第 K 大层和

--- src/medium/2583.二叉树中的第 K 大层和.ts
+++ src/medium/2583.二叉树中的第 K 大层和.ts
@@ -1,6 +1,6 @@
 import { TreeNode } from "@/utils";
 
-function kthLargestLevelSum(root: TreeNode | null, k: number): number {
+function kthLargestLevelSum$(root: TreeNode | null, k: number): number {
   let queue: TreeNode[] = root ? [root] : [];
   let level = 0;
   let sums: number[] = [];
@@ -23,6 +23,21 @@ function kthLargestLevelSum(root: TreeNode | null, k: number): number {
   sums.sort((a, b) => b - a);
 
   return sums[k - 1];
+}
+
+function kthLargestLevelSum(root: TreeNode | null, k: number): number {
+  const sumMap = new Map<number, number>();
+  dfs(root, 0);
+  if (sumMap.size < k) return -1;
+  return Array.from(sumMap.values()).sort((a, b) => b - a)[k - 1];
+
+  function dfs(node: TreeNode | null, depth: number) {
+    if (node === null) return;
+    sumMap.set(depth, (sumMap.get(depth) ?? 0) + node.val);
+    depth++;
+    dfs(node.left, depth);
+    dfs(node.right, depth);
+  }
 }
 
 export {};
