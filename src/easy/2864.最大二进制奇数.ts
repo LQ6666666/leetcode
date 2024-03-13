14a5da0d6074c3b513468baf4d7056368c41d2d1
李琦 <171035726@qq.com>
2024-03-13 15:23:02 +0800

    feat: 2864.最大二进制奇数

--- src/easy/2864.最大二进制奇数.ts
+++ src/easy/2864.最大二进制奇数.ts
@@ -1,4 +1,4 @@
-function maximumOddBinaryNumber(s: string): string {
+function maximumOddBinaryNumber$(s: string): string {
   const n = s.length;
   let count = 0;
 
@@ -21,6 +21,25 @@ function maximumOddBinaryNumber(s: string): string {
   }
 
   return arr.join("");
+}
+
+function maximumOddBinaryNumber(s: string): string {
+  const n = s.length;
+  const result = new Array<number>(n).fill(0);
+
+  let index = -1;
+  for (let i = 0; i < n; i++) {
+    if (s[i] === "1") {
+      if (index === -1) {
+        result[n - 1] = 1;
+      } else {
+        result[index] = 1;
+      }
+      index++;
+    }
+  }
+
+  return result.join("");
 }
 
 export {};
