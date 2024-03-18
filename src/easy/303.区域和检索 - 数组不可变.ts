9eaaddfbd9cb83c4c2ba174bf88e28c675d1a674
李琦 <171035726@qq.com>
2024-03-18 11:12:41 +0800

    feat: 303.区域和检索 - 数组不可变

--- src/easy/303.区域和检索 - 数组不可变.ts
+++ src/easy/303.区域和检索 - 数组不可变.ts
@@ -1,4 +1,4 @@
-class NumArray {
+class NumArray$ {
   private prefixNum: number[];
 
   constructor(nums: number[]) {
@@ -12,6 +12,24 @@ class NumArray {
 
   sumRange(left: number, right: number): number {
     return this.prefixNum[right + 1] - this.prefixNum[left];
+  }
+}
+
+class NumArray {
+  private prefix: number[];
+  constructor(nums: number[]) {
+    const prefix: number[] = new Array(nums.length + 1).fill(0);
+
+    const n = nums.length;
+    for (let i = 0; i <= n; i++) {
+      prefix[i + 1] = nums[i] + prefix[i];
+    }
+
+    this.prefix = prefix;
+  }
+
+  sumRange(left: number, right: number): number {
+    return this.prefix[right + 1] - this.prefix[left];
   }
 }
 
