06f6551c0813f9679a70d877019a990f524029d8
李琦 <171035726@qq.com>
2024-04-03 08:50:07 +0800

    feat: 1379.找出克隆二叉树中的相同节点

--- src/easy/1379.找出克隆二叉树中的相同节点.ts
+++ src/easy/1379.找出克隆二叉树中的相同节点.ts
@@ -7,7 +7,7 @@ function getTargetCopy(
 ): TreeNode | null {
   if (original === null) return null;
   if (original === target) return cloned;
-  const left = getTargetCopy(original!.left, cloned!.left, target);
+  const left = getTargetCopy(original.left, cloned!.left, target);
   if (left) return left;
   return getTargetCopy(original!.right, cloned!.right, target);
 }
