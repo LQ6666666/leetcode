51f6c18fe4bdba0535e1a75a612105f588f4c01b
lq <171035726@qq.com>
2024-04-24 23:31:53 +0800

    feat: 2385.感染二叉树需要的总时间

--- src/medium/2385.感染二叉树需要的总时间.ts
+++ src/medium/2385.感染二叉树需要的总时间.ts
@@ -1,6 +1,6 @@
 import { TreeNode } from "@/utils";
 
-function amountOfTime(root: TreeNode | null, start: number): number {
+function amountOfTime$(root: TreeNode | null, start: number): number {
   // 根据每个点的关联关系，建立图
   const g = new Map<number, number[]>();
   dfs(root, null);
@@ -49,5 +49,59 @@ function amountOfTime(root: TreeNode | null, start: number): number {
 
     from && relation.push(from.val);
     g.set(node.val, relation);
+  }
+}
+
+function amountOfTime(root: TreeNode | null, start: number): number {
+  const map = new Map<number, number[]>();
+  dfs(root, null);
+
+  const visitSet = new Set<number>();
+  let queue: number[] = [];
+
+  let ans = 0;
+  if (map.has(start)) {
+    queue = map.get(start)!;
+    visitSet.add(start);
+  }
+
+  while (queue.length) {
+    const newQueue: number[] = [];
+
+    for (const x of queue) {
+      visitSet.add(x);
+
+      const list = map.get(x) ?? [];
+      list.forEach(y => {
+        if (!visitSet.has(y)) {
+          newQueue.push(y);
+        }
+      });
+    }
+
+    ans++;
+    queue = newQueue;
+  }
+
+  return ans;
+
+  function dfs(node: TreeNode | null, from: number | null) {
+    if (node === null) return;
+
+    let list = map.get(node.val);
+    if (!list) {
+      map.set(node.val, (list = []));
+    }
+    if (from !== null) {
+      list.push(from);
+    }
+    if (node.left) {
+      dfs(node.left, node.val);
+      list.push(node.left.val);
+    }
+    if (node.right) {
+      dfs(node.right, node.val);
+      list.push(node.right.val);
+    }
   }
 }
