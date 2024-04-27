8ca91cc7be51061b5b26d2631432d2cdec92a8d1
lq <171035726@qq.com>
2024-04-27 22:37:23 +0800

    feat: 2639.查询网格图中每一列的宽度

--- src/easy/2639.查询网格图中每一列的宽度.ts
+++ src/easy/2639.查询网格图中每一列的宽度.ts
@@ -1,4 +1,4 @@
-function findColumnWidth(grid: number[][]): number[] {
+function findColumnWidth$(grid: number[][]): number[] {
   const m = grid.length;
   const n = grid[0].length;
 
@@ -12,5 +12,30 @@ function findColumnWidth(grid: number[][]): number[] {
   return ans;
 }
 
+function findColumnWidth(grid: number[][]): number[] {
+  const m = grid.length;
+  const n = grid[0].length;
+
+  const ans = new Array<number>(n).fill(0);
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      ans[j] = Math.max(helper(grid[i][j]), ans[j]);
+    }
+  }
+
+  return ans;
+}
+
+function helper(num: number): number {
+  let count = num <= 0 ? 1 : 0;
+  let x = Math.abs(num);
+
+  while (x > 0) {
+    count++;
+    x = Math.floor(x / 10);
+  }
+
+  return count;
+}
 
 export { findColumnWidth };
