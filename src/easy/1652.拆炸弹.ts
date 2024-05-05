6d08dc1be4e02bfbd57ed9c936a95d4c9bcf5808
lq <171035726@qq.com>
2024-05-05 21:10:07 +0800

    feat: 1652.拆炸弹

--- src/easy/1652.拆炸弹.ts
+++ src/easy/1652.拆炸弹.ts
@@ -1,4 +1,4 @@
-function decrypt(code: number[], k: number): number[] {
+function decrypt$(code: number[], k: number): number[] {
   const n = code.length;
   const ans = new Array<number>(n);
 
@@ -8,7 +8,8 @@ function decrypt(code: number[], k: number): number[] {
     for (let i = 0; i < n; i++) {
       let value = 0;
 
-      let count = k, j = i + 1;
+      let count = k,
+        j = i + 1;
       while (count-- > 0) {
         if (j === n) {
           j = 0;
@@ -21,7 +22,8 @@ function decrypt(code: number[], k: number): number[] {
     for (let i = 0; i < n; i++) {
       let value = 0;
 
-      let count = Math.abs(k), j = i - 1;
+      let count = Math.abs(k),
+        j = i - 1;
       while (count-- > 0) {
         if (j === -1) {
           j = n - 1;
@@ -35,5 +37,32 @@ function decrypt(code: number[], k: number): number[] {
   return ans;
 }
 
+function decrypt(code: number[], k: number): number[] {
+  const n = code.length;
+  const ans = new Array<number>(n).fill(0);
+  if (k === 0) return ans;
+
+  if (k > 0) {
+    for (let i = 0; i < n; i++) {
+      let sum = 0;
+      let index = 0;
+      while (index < k) {
+        sum += code[(index++ + i + 1) % n];
+      }
+      ans[i] = sum;
+    }
+  } else {
+    for (let i = 0; i < n; i++) {
+      let sum = 0;
+      let index = k;
+      while (index < 0) {
+        sum += code.at((index++ + i) % n)!;
+      }
+      ans[i] = sum;
+    }
+  }
+
+  return ans;
+}
 
 export { decrypt };
