59a2d781165295d48633feb5c94eaf61cd1796f5
lq <171035726@qq.com>
2024-09-15 22:22:21 +0800

    feat: 2848.与车相交的点

--- src/easy/2848.与车相交的点.ts
+++ src/easy/2848.与车相交的点.ts
@@ -1,4 +1,4 @@
-function numberOfPoints(nums: [start: number, end: number][]): number {
+function numberOfPoints$(nums: [start: number, end: number][]): number {
   // 合并区间
   nums.sort((a, b) => a[0] - b[0]);
   const ranges: [start: number, end: number][] = [nums[0]];
@@ -19,6 +19,26 @@ function numberOfPoints(nums: [start: number, end: number][]): number {
     ans += end - start + 1;
   }
 
+  return ans;
+}
+
+function numberOfPoints(nums: [start: number, end: number][]): number {
+  // 注意下面有 end + 1
+  const arr = new Array<number>(100 + 1 + 1).fill(0);
+
+  for (const [start, end] of nums) {
+    arr[start]++;
+    arr[end + 1]--;
+  }
+
+  let ans = 0;
+  let sum = 0;
+  for (let i = 1; i <= 100; i++) {
+    sum += arr[i];
+    if (sum > 0) {
+      ans++;
+    }
+  }
   return ans;
 }
 
