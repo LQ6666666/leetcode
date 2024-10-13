bf1a6439b9eea4698285ecccbad11c2a3b95e42b
lq <171035726@qq.com>
2024-10-13 22:31:04 +0800

    feat: 1887.使数组元素相等的减少操作次数

---
+++ src/1880-1889/1887.使数组元素相等的减少操作次数.ts
@@ -0,0 +1,17 @@
+function reductionOperations(nums: number[]): number {
+  const n = nums.length;
+  nums.sort((a, b) => a - b);
+
+  let ans = 0;
+  let count = 0;
+  for (let i = 1; i < n; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      count++;
+    }
+    ans += count;
+  }
+
+  return ans;
+}
+
+export { reductionOperations };
