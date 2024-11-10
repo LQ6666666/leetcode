5a7bbb9f4583ee095ed44fe711f02cb77a4785b8
lq <171035726@qq.com>
2024-11-10 21:18:10 +0800

    feat: 540.有序数组中的单一元素

--- src/540-549/540. 有序数组中的单一元素.ts
+++ src/540-549/540. 有序数组中的单一元素.ts
@@ -1,41 +1,27 @@
-// function singleNonDuplicate(nums: number[]): number {
-//     let i = 0, j = 1;
-//     const len = nums.length;
-
-//     while (j < len) {
-//         if (nums[i] !== nums[j]) {
-//             return nums[i];
-//         }
-
-//         i += 2;
-//         j += 2;
-//     }
-
-//     return nums[0];
-// };
-
 function singleNonDuplicate(nums: number[]): number {
-    let left = 0, right = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
 
-    while (left <= right) {
-        const middle = left + ((right - left) >> 1);
-
-        if (middle % 2 === 0) {
-            if (nums[middle + 1] === nums[middle]) {
-                left = middle + 1;
-            } else {
-                right = middle - 1;
-            }
-        } else {
-            if (nums[middle - 1] === nums[middle]) {
-                left = middle + 1;
-            } else {
-                right = middle - 1;
-            }
-        }
+  while (left <= right) {
+    const middle = left + ((right - left) >> 1);
+    if (middle % 2 === 0) {
+      if (nums[middle] === nums[middle + 1]) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    } else {
+      if (nums[middle] === nums[middle - 1]) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
     }
+  }
 
-    return left;
-};
+  return nums[left];
+}
 
-export { };
+export { singleNonDuplicate };
+
+export {};
