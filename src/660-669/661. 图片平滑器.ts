ede076fefffb504b007a755d0a0edf55010893b6
lq <171035726@qq.com>
2024-11-18 23:08:44 +0800

    feat: 661.图片平滑器

--- src/660-669/661. 图片平滑器.ts
+++ src/660-669/661. 图片平滑器.ts
@@ -1,26 +1,46 @@
 function imageSmoother(img: number[][]): number[][] {
-    const m = img.length, n = img[0].length;
-    const ans = Array.from({ length: m }, () => new Array<number>(n));
-    const positions = [[0, 0], [1, 0], [-1, 0], [0, 1], [0, -1], [-1, -1], [-1, 1], [1, -1], [1, 1]];
+  const m = img.length;
+  const n = img[0].length;
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            // 计算四周的平均值
-            let count = 0;
-            let sum = 0;
+  const ans = Array.from({ length: m }, () => new Array<number>(n));
 
-            for (const [x, y] of positions) {
-                let nx = i + x, ny = j + y;
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
-                    continue
-                }
-                sum += img[nx][ny];
-                count++;
-            }
-
-            ans[i][j] = Math.floor(sum / count);
-        }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      ans[i][j] = helper(img, i, j);
     }
+  }
 
-    return ans;
-};
+  return ans;
+}
+
+const Dir: [x: number, y: number][] = [
+  [0, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [-1, 0],
+  [-1, -1],
+  [0, -1],
+  [1, -1]
+];
+
+function helper(img: number[][], i: number, j: number): number {
+  const m = img.length;
+  const n = img[0].length;
+
+  let ret = 0;
+  let count = 0;
+  for (const [x, y] of Dir) {
+    const dx = j + x;
+    const dy = i + y;
+    if (dx >= 0 && dx < n && dy >= 0 && dy < m) {
+      ret += img[dy][dx];
+      count++;
+    }
+  }
+
+  return Math.floor(ret / count);
+}
+
+export { imageSmoother };
