94f6b32af0833c4eaa062b30fbe3bbec690203ce
lq <171035726@qq.com>
2025-02-06 22:26:28 +0800

    feat: 47.全排列 II

--- src/40-49/47.全排列 II.ts
+++ src/40-49/47.全排列 II.ts
@@ -30,37 +30,38 @@ function permuteUnique1(nums: number[]): number[][] {
 // 剪枝
 function permuteUnique(nums: number[]): number[][] {
   const n = nums.length;
-  const result: number[][] = [];
-  let path: number[] = [];
-  const map = new Map();
+  const ans: number[][] = [];
+  const path: number[] = [];
+  const visits = new Array<0 | 1>(n).fill(0);
+
+  nums.sort((a, b) => a - b);
+  dfs();
+  return ans;
 
   function dfs() {
     if (path.length === nums.length) {
-      result.push(path.slice());
+      ans.push(path.slice());
+      return;
     }
 
     for (let i = 0; i < n; i++) {
-      if (map.get(i)) continue;
+      if (visits[i] === 1) continue;
 
       // 如果有前一个数 && 当前数和前一个数相等 && 前一个数没被访问过
       // 上一个撤销的也是同样的数，可以剪枝
-      if (i > 0 && nums[i] == nums[i - 1] && map.get(i - 1) === false) {
+      if (i > 0 && nums[i] == nums[i - 1] && visits[i - 1] === 0) {
         continue;
       }
 
-      map.set(i, true);
+      visits[i] = 1;
       path.push(nums[i]);
 
       dfs();
 
       path.pop();
-      map.set(i, false);
+      visits[i] = 0;
     }
   }
-
-  nums.sort((a, b) => a - b);
-  dfs();
-  return result;
 }
 
 export {};
