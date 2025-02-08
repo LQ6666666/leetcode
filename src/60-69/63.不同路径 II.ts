5d4b7e5cf63df3aa7afc23694ce050ca3345a603
lq <171035726@qq.com>
2025-02-08 21:49:48 +0800

    feat: 63.不同路径 II

--- src/60-69/63.不同路径 II.ts
+++ src/60-69/63.不同路径 II.ts
@@ -1,24 +1,21 @@
-// dfs + 缓存
 function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   const m = obstacleGrid.length;
   const n = obstacleGrid[0].length;
-  const cache = new Map<string, number>();
+  const memo = Array.from({ length: m }, () => new Array<number>(n));
 
   return dfs(0, 0);
 
   function dfs(i: number, j: number): number {
     if (i >= m || j >= n) return 0;
-    if (obstacleGrid[i][j]) return 0;
+    if (obstacleGrid[i][j] === 1) return 0;
 
     if (i === m - 1 && j === n - 1) return 1;
 
-    const cacheKey = `${i},${j}`;
-    const exist = cache.get(cacheKey);
+    const exist = memo[i][j];
     if (exist !== undefined) return exist;
 
     const result = dfs(i + 1, j) + dfs(i, j + 1);
-    cache.set(cacheKey, result);
-    return result;
+    return (memo[i][j] = result);
   }
 }
 
