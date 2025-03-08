b5a7205234eb9a5e468e7b5b8934cdff9abf98f1
lq <171035726@qq.com>
2025-03-08 22:04:55 +0800

    feat: 865.具有所有最深节点的最小子树

---
+++ src/860-869/865.具有所有最深节点的最小子树.ts
@@ -0,0 +1,23 @@
+import { TreeNode } from "@/utils";
+
+function subtreeWithAllDeepest(root: TreeNode | null): TreeNode | null {
+  if (root === null) return null;
+
+  const leftDepth = depth(root.left);
+  const rightDepth = depth(root.right);
+
+  if (leftDepth === rightDepth) {
+    return root;
+  } else if (leftDepth > rightDepth) {
+    return subtreeWithAllDeepest(root.left);
+  } else {
+    return subtreeWithAllDeepest(root.right);
+  }
+}
+
+function depth(node: TreeNode | null): number {
+  if (node === null) return 0;
+  return Math.max(depth(node.left), depth(node.right)) + 1;
+}
+
+export { subtreeWithAllDeepest };
