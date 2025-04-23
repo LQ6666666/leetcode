82177eae34026a593534e6cd4bfbda7d2c67021d
李琦 <171035726@qq.com>
2025-04-23 13:53:54 +0800

    feat: 1399.统计最大组的数目

--- src/1390-1399/1399.统计最大组的数目.ts
+++ src/1390-1399/1399.统计最大组的数目.ts
@@ -5,11 +5,10 @@ function countLargestGroup(n: number): number {
   for (let i = 1; i <= n; i++) {
     let x = i;
     let sum = 0;
-    while (x >= 10) {
+    while (x > 0) {
       sum += x % 10;
       x = Math.floor(x / 10);
     }
-    sum += x;
     counts[sum]++;
     max = Math.max(counts[sum], max);
   }
