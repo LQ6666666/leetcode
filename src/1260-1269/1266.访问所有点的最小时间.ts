32b3d23417e2d4f0abc5e933ab83723d78b44cc1
lq <171035726@qq.com>
2025-06-26 22:35:19 +0800

    feat: 1266.访问所有点的最小时间

---
+++ src/1260-1269/1266.访问所有点的最小时间.ts
@@ -0,0 +1,15 @@
+function minTimeToVisitAllPoints(points: number[][]): number {
+  const n = points.length;
+  let ans = 0;
+
+  for (let i = 1; i < n; i++) {
+    ans += Math.max(
+      Math.abs(points[i][0] - points[i - 1][0]),
+      Math.abs(points[i][1] - points[i - 1][1])
+    );
+  }
+
+  return ans;
+}
+
+export { minTimeToVisitAllPoints };
