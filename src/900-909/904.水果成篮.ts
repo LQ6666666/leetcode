7615e4816dfb6917a0897d7934faebed757134d9
lq <171035726@qq.com>
2025-08-04 23:00:51 +0800

    feat: 904.水果成篮

--- src/900-909/904.水果成篮.ts
+++ src/900-909/904.水果成篮.ts
@@ -1,42 +1,27 @@
 function totalFruit(fruits: number[]): number {
   const n = fruits.length;
-  if (n === 0) return 0;
 
-  let ans = 0;
   const map = new Map<number, number>();
-  let left = 0,
-    right = 0;
-
-  while (right < n) {
-    const fruit = fruits[right];
-    setCount(fruit, getCount(fruit) + 1);
+  let ans = 0;
+  for (let left = 0, right = 0; right < n; right++) {
+    map.set(fruits[right], (map.get(fruits[right]) ?? 0) + 1);
 
     // 超出两种了
     while (map.size > 2) {
-      setCount(fruits[left], getCount(fruits[left]) - 1);
+      let count = map.get(fruits[left]) ?? 0;
+      count--;
+      if (count <= 0) {
+        map.delete(fruits[left]);
+      } else {
+        map.set(fruits[left], count);
+      }
       left++;
     }
 
     ans = Math.max(ans, right - left + 1);
-    right++;
   }
 
   return ans;
-
-  function getCount(key: number) {
-    return map.get(key) ?? 0;
-  }
-
-  function setCount(key: number, value: number) {
-    if (value < 1) {
-      return map.delete(key);
-    } else {
-      return map.set(key, value);
-    }
-  }
 }
-
-// console.log(totalFruit([1, 2, 1]));
-console.log(totalFruit([0, 1, 2, 2]));
 
 export { totalFruit };
