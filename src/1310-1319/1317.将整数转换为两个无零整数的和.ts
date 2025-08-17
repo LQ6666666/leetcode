7d40078f594d17ae2017db2944a403cac3772af6
lq <171035726@qq.com>
2025-08-17 21:44:24 +0800

    feat: 1317.将整数转换为两个无零整数的和

---
+++ src/1310-1319/1317.将整数转换为两个无零整数的和.ts
@@ -0,0 +1,12 @@
+function getNoZeroIntegers(n: number): number[] {
+  for (let i = 1; i < n; i++) {
+    const a = i;
+    const b = n - i;
+    if (!a.toString().includes("0") && !b.toString().includes("0")) {
+      return [a, b];
+    }
+  }
+  return [0, n];
+}
+
+export { getNoZeroIntegers };
