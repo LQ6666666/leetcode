ce6adfcd77b1af8d7fe5839aced10fb969b490eb
lq <171035726@qq.com>
2025-10-08 14:47:02 +0800

    feat: 1486.数组异或操作

---
+++ src/1480-1489/1486.数组异或操作.ts
@@ -0,0 +1,9 @@
+function xorOperation(n: number, start: number): number {
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    ans ^= start + 2 * i;
+  }
+  return ans;
+}
+
+export { xorOperation };
