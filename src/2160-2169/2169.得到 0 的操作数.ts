2b4037a832cec13b12be33d4d1ffec7710a85578
lq <171035726@qq.com>
2025-11-09 21:17:20 +0800

    feat: 2169.得到 0 的操作数

---
+++ src/2160-2169/2169.得到 0 的操作数.ts
@@ -0,0 +1,14 @@
+function countOperations(num1: number, num2: number): number {
+  let ans = 0;
+  while (num1 > 0 && num2 > 0) {
+    if (num1 >= num2) {
+      num1 -= num2;
+    } else {
+      num2 -= num1;
+    }
+    ans++;
+  }
+  return ans;
+}
+
+export {};
