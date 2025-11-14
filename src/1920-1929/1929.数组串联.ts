aae7393807ac160d255d90940e9734526c3fccf5
lq <171035726@qq.com>
2025-11-14 22:36:48 +0800

    feat: 1929.数组串联

--- src/1920-1929/1929.数组串联.ts
+++ src/1920-1929/1929.数组串联.ts
@@ -1,5 +1,10 @@
 function getConcatenation(nums: number[]): number[] {
-  return nums.concat(nums);
+  const n = nums.length;
+  const ans = new Array<number>(n * 2);
+  for (let i = 0; i < n; i++) {
+    ans[i] = ans[n + i] = nums[i];
+  }
+  return ans;
 }
 
 export { getConcatenation };
