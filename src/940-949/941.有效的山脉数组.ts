24ba7a70acab3e30f4e960907909ab203582b31c
lq <171035726@qq.com>
2025-11-20 21:57:33 +0800

    feat: 941.有效的山脉数组

--- src/940-949/941.有效的山脉数组.ts
+++ src/940-949/941.有效的山脉数组.ts
@@ -1,21 +1,19 @@
 function validMountainArray(arr: number[]): boolean {
   const n = arr.length;
-  if (n < 3) return false;
-
-  let flag = 0;
-  for (let i = 1; i < n; i++) {
-    if (flag === 0) {
-      if (arr[i] <= arr[i - 1]) {
-        flag = 1;
-      }
-    } else {
-      if (arr[i] >= arr[i - 1]) {
-        return false;
-      }
-    }
+  let i = 0;
+  while (i + 1 < n && arr[i] < arr[i + 1]) {
+    i++;
   }
 
-  return arr[0] < arr[1] && arr[n - 2] > arr[n - 1];
+  if (i === 0 || i === n - 1) {
+    return false;
+  }
+
+  while (i + 1 < n && arr[i] > arr[i + 1]) {
+    i++;
+  }
+
+  return i === n - 1;
 }
 
-export { validMountainArray };
+export {};
