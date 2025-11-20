ba463eac485e718378d9490fb91867bd8c8c91d6
lq <171035726@qq.com>
2025-11-20 21:49:13 +0800

    feat: 66. 加一

--- src/60-69/66. 加一.ts
+++ src/60-69/66. 加一.ts
@@ -1,50 +1,21 @@
-
 function plusOne(digits: number[]): number[] {
-    const digitsLength: number = digits.length;
+  const n = digits.length;
 
-    if (!digitsLength) {
-        return [];
+  digits[n - 1]++;
+  for (let i = n - 1; i >= 0; i--) {
+    if (digits[i] >= 10) {
+      digits[i] %= 10;
+      if (i - 1 >= 0) {
+        digits[i - 1]++;
+      } else {
+        digits.unshift(1);
+      }
+    } else {
+      break;
     }
+  }
 
-    // 最后一位小于 9 ，表明没有进位
-    if (digits[digitsLength - 1] < 9) {
-        const newArr: number[] = [...digits];
-
-        newArr.push(newArr.pop()! + 1);
-
-        return newArr;
-    }
-
-    // 到这里表示有进位
-    digits[digitsLength - 1]++;
-
-    for (let i = digits.length - 1; i > -1; i--) {
-        if (digits[i] > 9) {
-            digits[i] = digits[i] - 10;
-
-            // 前一位是否有值
-            if (digits[i - 1]) {
-                digits[i - 1]++;
-            } else {
-                digits.unshift(1);
-                break;
-            }
-        } else {
-            break;
-        }
-    }
-
-    return digits;
-};
-
-
-// console.log(plusOne([1, 9, 9]))
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]))
-
-
-
-
-
-export {
-    plusOne
+  return digits;
 }
+
+export { plusOne };
