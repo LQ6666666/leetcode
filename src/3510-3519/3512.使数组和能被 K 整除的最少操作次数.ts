049bd4bbbe08d9be56a6e0ad0931becbfd82798d
lq <171035726@qq.com>
2025-11-29 14:44:48 +0800

    feat: 3512.使数组和能被 K 整除的最少操作次数

--- src/3510-3519/3512.使数组和能被 K 整除的最少操作次数.ts
+++ src/3510-3519/3512.使数组和能被 K 整除的最少操作次数.ts
@@ -1,13 +1,5 @@
 function minOperations(nums: number[], k: number): number {
-  let ans = 0;
-
-  let num = nums.reduce((memo, x) => memo + x, 0);
-  while (num % k !== 0) {
-    ans++;
-    num--;
-  }
-
-  return ans;
+  return nums.reduce((memo, x) => memo + x, 0) % k;
 }
 
 export { minOperations };
