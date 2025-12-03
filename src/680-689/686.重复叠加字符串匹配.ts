bda628a304f20a7808d62aa0edf14408e590b986
lq <171035726@qq.com>
2025-12-03 22:57:32 +0800

    feat: 686.重复叠加字符串匹配

--- src/680-689/686.重复叠加字符串匹配.ts
+++ src/680-689/686.重复叠加字符串匹配.ts
@@ -5,9 +5,9 @@ function repeatedStringMatch(a: string, b: string): number {
     if (!set.has(ch)) return -1;
   }
 
-  let count = Math.floor(b.length / a.length);
+  let count = Math.ceil(b.length / a.length);
   let s = a.repeat(count);
-  for (let i = 0; i <= 2; i++) {
+  for (let i = 0; i < 2; i++) {
     if (s.indexOf(b) > -1) {
       return count + i;
     }
