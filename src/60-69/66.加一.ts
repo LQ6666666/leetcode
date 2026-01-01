89871ddd0476e6364f168bb963fda7122758cf40
lq <171035726@qq.com>
2026-01-01 23:19:39 +0800

    feat: 66.加一

--- src/60-69/66.加一.ts
+++ src/60-69/66.加一.ts
@@ -1,17 +1,15 @@
 function plusOne(digits: number[]): number[] {
   const n = digits.length;
+  digits[n - 1] += 1;
 
-  digits[n - 1]++;
   for (let i = n - 1; i >= 0; i--) {
-    if (digits[i] >= 10) {
-      digits[i] %= 10;
-      if (i - 1 >= 0) {
-        digits[i - 1]++;
-      } else {
+    if (digits[i] === 10) {
+      digits[i] = 0;
+      if (i === 0) {
         digits.unshift(1);
+      } else {
+        digits[i - 1]++;
       }
-    } else {
-      break;
     }
   }
 
