41ba80a8ff7789dda2fe483cdf3550a10e6f7e12
lq <171035726@qq.com>
2026-03-05 22:02:36 +0800

    feat: LCR 088.使用最小花费爬楼梯

---
+++ src/LCR/LCR 088.使用最小花费爬楼梯.ts
@@ -0,0 +1,13 @@
+function minCostClimbingStairs(cost: number[]): number {
+  // dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+  const n = cost.length;
+  const dp = new Array<number>(n + 1);
+  dp[0] = 0;
+  dp[1] = 0;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+  }
+  return dp[n];
+}
+
+export {};
