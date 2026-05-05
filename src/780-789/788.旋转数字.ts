94c0443d209eaf692005d843dbf02a2f919c86c0
lq <171035726@qq.com>
2026-05-05 22:33:07 +0800

    788.旋转数字

--- src/780-789/788.旋转数字.ts
+++ src/780-789/788.旋转数字.ts
@@ -7,7 +7,7 @@ function rotatedDigits(n: number): number {
     for (const s of i.toString()) {
       if (s === "3" || s === "4" || s === "7") {
         flag = false;
-        break;;
+        break;
       }
 
       if (s === "2" || s === "5" || s === "6" || s === "9") {
@@ -20,35 +20,7 @@ function rotatedDigits(n: number): number {
     }
   }
 
-
   return ans;
-};
-
-function rotatedDigits1(n: number): number {
-  const FAIL_CHS = new Set([3, 4, 7]);
-  const SUCCESS_CHS = new Set([2, 5, 6, 9]);
-
-  let ans = 0;
-
-  for (let i = 1; i <= n; i++) {
-    let flag = false;
-    for (const ch of i.toString()) {
-      if (FAIL_CHS.has(+ch)) {
-        flag = false;
-        break;
-      }
-
-      if (SUCCESS_CHS.has(+ch)) {
-        flag = true;
-      }
-    }
-
-    if (flag) {
-      ans++;
-    }
-  }
-
-  return ans;
-};
+}
 
 export { rotatedDigits };
