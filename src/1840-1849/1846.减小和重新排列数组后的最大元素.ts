b125ef65edb06f252e17d4f572dd8b582ced10fb
lq <171035726@qq.com>
2026-06-28 23:05:00 +0800

    1846.减小和重新排列数组后的最大元素

---
+++ src/1840-1849/1846.减小和重新排列数组后的最大元素.ts
@@ -0,0 +1,15 @@
+function maximumElementAfterDecrementingAndRearranging(arr: number[]): number {
+  const n = arr.length;
+  arr.sort((a, b) => a - b);
+
+  arr[0] = 1;
+  for (let i = 1; i < n; i++) {
+    if (arr[i] - arr[i - 1] > 1) {
+      arr[i] = arr[i - 1] + 1;
+    }
+  }
+
+  return arr[n - 1];
+}
+
+export {};
