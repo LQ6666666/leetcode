beb749e04aee3fa63efb85bc98606a21350a250d
lq <171035726@qq.com>
2026-07-08 23:18:45 +0800

    3754.连接非零数字并乘以其数字和 I

--- src/3750-3759/3754.连接非零数字并乘以其数字和 I.ts
+++ src/3750-3759/3754.连接非零数字并乘以其数字和 I.ts
@@ -1,20 +1,16 @@
 function sumAndMultiply(n: number): number {
   let sum = 0;
-  const nums: number[] = [];
 
+  let y = 1;
+  let x = 0;
   while (n) {
     const num = n % 10;
     if (num) {
-      nums.push(num);
+      x += num * y;
+      y *= 10;
       sum += num;
     }
     n = Math.floor(n / 10);
-  }
-
-  let x = 0;
-  nums.reverse();
-  for (const num of nums) {
-    x = x * 10 + num;
   }
 
   return x * sum;
